6fdaa70c27e6a8cfd3919fcecabb3cdd818b7d0b
Ivan Volgin <volgin.ivan.05@gmail.com>
2025-02-01 22:41:31 +0300

    writed main func and fixed http_server.NewServer

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,14 +1,30 @@
 package main
 
-// import(
-// 	"log"
-// 	"net/http"
-// 	"todo-service/internal/interfaces/repositories"
-//     "todo-service/internal/usecases"
-//     "todo-service/pkg/database"
-//     "todo-service/pkg/http"
-// )
+import (
+	"log"
+	"net/http"
+	"todo-service/internal/interfaces/handlers"
+	"todo-service/internal/interfaces/repositories"
+	"todo-service/internal/usecases"
+	"todo-service/pkg/database"
+	"todo-service/pkg/http_server"
+)
 
 func main(){
-	
+	db, err := database.Conect("", "", "", "", "")
+	if err != nil {
+		log.Fatalf("Could not connect to database %v", err)
+	}
+	defer db.Close()
+
+	taskRepo := repositories.NewPostgresTaskRepository(db)
+
+	taskUseCase := usecases.NewTaskUseCase(taskRepo)
+
+	taskHandler := handlers.NewTaskHandler(taskUseCase)
+
+	r := http_server.NewServer(*taskHandler)
+
+	log.Println("Server is running on port 8080...")
+    log.Fatal(http.ListenAndServe(":8080", r))
 }
